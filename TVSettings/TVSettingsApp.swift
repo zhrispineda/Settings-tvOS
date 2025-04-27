28f43e11650e98398fc7bed7cebbfd793abe3109
Chris <zhrispineda@icloud.com>
2025-04-27 14:21:46 -0500

    Initial Commit

---
+++ TVSettings/TVSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVSettingsApp.swift
+//  TVSettings
+//
+//  Created by Chris on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TVSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
