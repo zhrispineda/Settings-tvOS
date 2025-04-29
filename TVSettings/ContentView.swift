4c7383c80d2a6bf7e934cb4996f6c3eb139a5320
Chris <zhrispineda@icloud.com>
2025-04-29 11:27:26 -0500

    Add options

--- TVSettings/ContentView.swift
+++ TVSettings/ContentView.swift
@@ -7,14 +7,34 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.colorScheme) private var colorScheme
-    
+
     var body: some View {
         NavigationStack {
             HStack {
                 Image(.settingsAtv2Device)
                     .foregroundStyle(Color(UIColor.quaternaryLabel))
                     .frame(width: UIScreen.main.bounds.width * 0.45)
-                Form {}
+                
+                ZStack {
+                    Form {
+                        NavigationLink("General") {}
+                        NavigationLink("Users and Accounts") {}
+                        NavigationLink("Video and Audio") {}
+                        NavigationLink("Screen Saver") {}
+                        NavigationLink("AirPlay and HomeKit") {}
+                        NavigationLink("Remotes and Devices") {}
+                        NavigationLink("Accessibility") {}
+                        NavigationLink("Apps") {}
+                        NavigationLink("Network") {}
+                        NavigationLink("System") {}
+                        NavigationLink("Developer") {}
+                        Button("Sleep Now") {}
+                    }
+                    .contentMargins(30, for: .scrollContent)
+                    .frame(height: 800)
+                    .padding(.top, 100)
+                    ._safeAreaInsets(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
+                }
             }
             .navigationTitle("Settings")
         }
