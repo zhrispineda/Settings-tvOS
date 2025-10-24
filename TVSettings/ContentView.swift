5755d8233edfb7e608479f50c8f2c947591e73a0
zhrispineda <zhris@zhr.is>
2025-10-24 19:05:02 -0400

    Clear out ContentView

--- TVSettings/ContentView.swift
+++ TVSettings/ContentView.swift
@@ -7,20 +7,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        SettingsView(title: "Settings") {
-            NavigationLink("GeneralTitle") {}
-            NavigationLink("ProfilesTitle") {}
-            NavigationLink("AudioVideoTitle") {}
-            NavigationLink("ScreenSaverTitle") {}
-            NavigationLink("AirPlayTitle") {}
-            NavigationLink("RemotesTitle") {}
-            NavigationLink("AccessibilityTitle") {}
-            NavigationLink("AppsTitle") {}
-            NavigationLink("NetworkTitle") {}
-            NavigationLink("SystemTitle") {}
-            NavigationLink("DeveloperTitle") {}
-            Button("SleepTitle") {}
-        }
+        EmptyView()
     }
 }
 
