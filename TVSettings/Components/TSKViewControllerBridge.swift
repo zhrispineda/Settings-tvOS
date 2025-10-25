e66403f799e21ea1ae9142090b897ae80161637c
zhrispineda <zhris@zhr.is>
2025-10-24 22:06:47 -0400

    Add TSKViewControllerBridge and TSKViewWrapper

---
+++ TVSettings/Components/TSKViewControllerBridge.swift
@@ -0,0 +1,91 @@
+//
+//  TSKViewControllerBridge.swift
+//  TVSettings
+//
+
+import SwiftUI
+
+class TSKViewControllerBridge: NSObject {
+    private static var groupsMap: [String: NSArray] = [:]
+    static var previewMapMap: [String: NSMutableDictionary] = [:]
+    
+    static func registerSubclass(with groups: [NSObject]) -> AnyClass? {
+        guard let baseClass = NSClassFromString("TSKViewController") else { return nil }
+
+        let subclassName = "TSKViewController_\(UUID().uuidString.prefix(8))"
+        guard let subclass = objc_allocateClassPair(baseClass, subclassName, 0) else { return nil }
+
+        let className = NSStringFromClass(subclass)
+        groupsMap[className] = groups as NSArray
+
+        let loadSel = NSSelectorFromString("loadSettingGroups")
+        let loadIMP: @convention(block) (AnyObject) -> NSArray? = { instance in
+            let className = NSStringFromClass(type(of: instance))
+            return Self.groupsMap[className]
+        }
+        
+        let previewSel = NSSelectorFromString("previewForItemAtIndexPath:")
+        let previewIMP: @convention(block) (AnyObject, AnyObject) -> AnyObject? = { instance, indexPath in
+            let className = NSStringFromClass(type(of: instance))
+            guard let previewMap = Self.previewMapMap[className] else { return nil }
+            
+            let getItemSel = NSSelectorFromString("settingItemAtIndexPath:")
+            guard let obj = instance as? NSObject else { return nil }
+            guard let item = obj.perform(getItemSel, with: indexPath)?.takeUnretainedValue() as? NSObject else {
+                return nil
+            }
+            
+            let itemKey = NSValue(nonretainedObject: item)
+            let previewImageName = previewMap.object(forKey: itemKey) as? String ?? "settings_atv2_device"
+            let description = item.value(forKey: "localizedDescription") as? String
+            
+            return Self.createPreviewViewController(with: previewImageName, description: description)
+        }
+        
+        class_addMethod(subclass, loadSel, imp_implementationWithBlock(loadIMP), "@@:")
+        class_addMethod(subclass, previewSel, imp_implementationWithBlock(previewIMP), "@@:@")
+        
+        return subclass
+    }
+    
+    static func create(with groups: [NSObject], model: [TSKViewWrapper.SettingGroupData], previewMap: NSMutableDictionary) -> UIViewController? {
+        guard let subclass = registerSubclass(with: groups) else { return nil }
+        
+        let className = NSStringFromClass(subclass)
+        previewMapMap[className] = previewMap
+        
+        guard
+            let subclassAlloc = (subclass as! NSObject.Type).perform(NSSelectorFromString("alloc"))?.takeUnretainedValue() as? NSObject,
+            let instance = subclassAlloc.perform(NSSelectorFromString("init"))?.takeUnretainedValue() as? UIViewController
+        else { return nil }
+        
+        return instance
+    }
+    
+    private static func createPreviewViewController(with imageName: String, description: String?) -> UIViewController? {
+        guard let image = UIImage(named: imageName) else { return nil }
+        
+        guard
+            let vibrantImageViewClass = NSClassFromString("TSKVibrantImageView") as? NSObject.Type,
+            let vibrantAlloc = vibrantImageViewClass.perform(NSSelectorFromString("alloc"))?.takeUnretainedValue() as? NSObject,
+            let vibrantView = vibrantAlloc.perform(NSSelectorFromString("initWithImage:identifier:"), with: image, with: imageName as NSString)?.takeUnretainedValue() as? NSObject
+        else {
+            return nil
+        }
+
+        guard
+            let previewClass = NSClassFromString("TSKPreviewViewController") as? NSObject.Type,
+            let previewAlloc = previewClass.perform(NSSelectorFromString("alloc"))?.takeUnretainedValue() as? NSObject,
+            let previewVC = previewAlloc.perform(NSSelectorFromString("init"))?.takeUnretainedValue() as? UIViewController
+        else {
+            return nil
+        }
+
+        previewVC.perform(NSSelectorFromString("setContentView:"), with: vibrantView)
+        if let desc = description {
+            previewVC.perform(NSSelectorFromString("setDescriptionText:"), with: desc as NSString)
+        }
+        
+        return previewVC
+    }
+}
---
+++ TVSettings/Components/TSKViewWrapper.swift
@@ -0,0 +1,230 @@
+//
+//  TSKViewControllerBridge.swift
+//  TVSettings
+//
+
+import SwiftUI
+
+struct TSKViewWrapper: UIViewControllerRepresentable {
+    let navigationTitle: String?
+    let settingGroups: [SettingGroupData]
+    
+    struct SettingGroupData {
+        let title: String?
+        let items: [SettingItemData]
+    }
+    
+    struct SettingItemData {
+        let title: String
+        let accessoryType: AccessoryType
+        let action: (() -> Void)?
+        let subviews: [SettingGroupData]?
+        let previewDescription: String?
+        let previewImageName: String
+        let badgeCount: Int?
+        
+        enum AccessoryType {
+            case none
+            case disclosureIndicator
+        }
+        
+        init(
+            title: String,
+            accessoryType: AccessoryType = .none,
+            action: (() -> Void)? = nil,
+            subviews: [SettingGroupData]? = nil,
+            previewDescription: String? = nil,
+            previewImageName: String = "settings_atv2_device",
+            badgeCount: Int = 0
+        ) {
+            self.title = title
+            self.accessoryType = accessoryType
+            self.action = action
+            self.subviews = subviews
+            self.previewDescription = previewDescription
+            self.previewImageName = previewImageName
+            self.badgeCount = badgeCount
+        }
+    }
+    
+    init(
+        navigationTitle: String? = nil,
+        settingGroups: [SettingGroupData] = []
+    ) {
+        self.navigationTitle = navigationTitle
+        self.settingGroups = settingGroups
+    }
+    
+    init(
+        _ navigationTitle: String? = nil,
+        @SettingGroupBuilder builder: () -> [SettingGroupData]
+    ) {
+        self.navigationTitle = navigationTitle
+        self.settingGroups = builder()
+    }
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        let previewMap = NSMutableDictionary()
+        let groups = buildGroupsRecursivelyWithImages(from: settingGroups, coordinator: context.coordinator, previewMap: previewMap)
+        
+        guard let tskVC = TSKViewControllerBridge.create(with: groups, model: settingGroups, previewMap: previewMap) else {
+            return UIViewController()
+        }
+        
+        if let title = navigationTitle {
+            tskVC.title = title
+        }
+        
+        let navController = UINavigationController(rootViewController: tskVC)
+        
+        guard let splitViewClass = NSClassFromString("_TSKSplitViewController") as? NSObject.Type else {
+            return navController
+        }
+        
+        guard let splitVCAlloc = splitViewClass.perform(NSSelectorFromString("alloc"))?.takeUnretainedValue() as? NSObject else {
+            return navController
+        }
+        
+        let splitVC = splitVCAlloc.perform(
+            NSSelectorFromString("initWithNavigationController:"),
+            with: navController
+        )?.takeUnretainedValue() as? UIViewController
+        
+        let finalVC = splitVC ?? navController
+
+        return finalVC
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    private func buildGroupsRecursivelyWithImages(from groupsData: [SettingGroupData], coordinator: Coordinator, previewMap: NSMutableDictionary) -> [NSObject] {
+        var groups: [NSObject] = []
+        
+        for groupData in groupsData {
+            var items: [NSObject] = []
+            
+            for itemData in groupData.items {
+                if let subviews = itemData.subviews, !subviews.isEmpty {
+                    let childPreviewMap = NSMutableDictionary()
+                    let subGroups = buildGroupsRecursivelyWithImages(from: subviews, coordinator: coordinator, previewMap: childPreviewMap)
+                    
+                    guard let childClass = TSKViewControllerBridge.registerSubclass(with: subGroups) else {
+                        continue
+                    }
+                    
+                    let childClassName = NSStringFromClass(childClass)
+                    TSKViewControllerBridge.previewMapMap[childClassName] = childPreviewMap
+                    
+                    if let childItem = TVSettingKitBridge.shared.createChildPaneItem(
+                        title: itemData.title,
+                        description: itemData.previewDescription,
+                        childControllerClass: childClass
+                    ) {
+                        childItem.setValue(itemData.previewDescription, forKey: "localizedDescription")
+                        previewMap.setObject(itemData.previewImageName, forKey: NSValue(nonretainedObject: childItem))
+                        items.append(childItem)
+                    }
+                } else if itemData.accessoryType == .disclosureIndicator {
+                    let childPreviewMap = NSMutableDictionary()
+                    let emptyGroup = TVSettingKitBridge.shared.createSettingGroup(title: "", items: [])
+                    guard let emptyGroup = emptyGroup else { continue }
+                    
+                    guard let childClass = TSKViewControllerBridge.registerSubclass(with: [emptyGroup]) else {
+                        continue
+                    }
+                    
+                    let childClassName = NSStringFromClass(childClass)
+                    TSKViewControllerBridge.previewMapMap[childClassName] = childPreviewMap
+                    
+                    if let childItem = TVSettingKitBridge.shared.createChildPaneItem(
+                        title: itemData.title,
+                        description: itemData.previewDescription,
+                        childControllerClass: childClass
+                    ) {
+                        childItem.setValue(itemData.previewDescription, forKey: "localizedDescription")
+                        previewMap.setObject(itemData.previewImageName, forKey: NSValue(nonretainedObject: childItem))
+                        items.append(childItem)
+                    }
+                } else if let actionClosure = itemData.action {
+                    if let actionItem = TVSettingKitBridge.shared.createActionItem(
+                        title: itemData.title,
+                        description: itemData.previewDescription,
+                        target: coordinator,
+                        action: #selector(Coordinator.handleActionItem(_:))
+                    ) {
+                        coordinator.registerAction(for: actionItem, action: actionClosure)
+                        items.append(actionItem)
+                    }
+                } else {
+                    if let item = TVSettingKitBridge.shared.createSettingItem(
+                        title: itemData.title,
+                        description: itemData.previewDescription,
+                        representedObject: nil,
+                        keyPath: nil
+                    ) {
+                        previewMap.setObject(itemData.previewImageName, forKey: NSValue(nonretainedObject: item))
+                        items.append(item)
+                    }
+                }
+            }
+            
+            if let group = TVSettingKitBridge.shared.createSettingGroup(
+                title: groupData.title ?? "",
+                items: items
+            ) {
+                groups.append(group)
+            }
+        }
+        
+        return groups
+    }
+    
+    class Coordinator {
+        var viewController: UIViewController?
+        var navigationController: UINavigationController?
+        var groups: [NSObject] = []
+        
+        private var actionMap: NSMapTable<AnyObject, AnyObject> = NSMapTable.weakToStrongObjects()
+        
+        func registerAction(for item: NSObject, action: @escaping () -> Void) {
+            actionMap.setObject(ActionBox(action), forKey: item)
+        }
+        
+        @objc func handleActionItem(_ sender: Any) {
+            guard let senderObj = sender as AnyObject?,
+                  let box = actionMap.object(forKey: senderObj) as? ActionBox else { return }
+            box.action()
+        }
+        
+        private class ActionBox: NSObject {
+            let action: () -> Void
+            init(_ action: @escaping () -> Void) { self.action = action }
+        }
+    }
+}
+
+// MARK: - Result Builders
+@resultBuilder
+struct SettingGroupBuilder {
+    static func buildBlock(_ components: TSKViewWrapper.SettingGroupData...) -> [TSKViewWrapper.SettingGroupData] {
+        components
+    }
+}
+
+@resultBuilder
+struct SettingItemBuilder {
+    static func buildBlock(_ components: TSKViewWrapper.SettingItemData...) -> [TSKViewWrapper.SettingItemData] {
+        components
+    }
+}
+
+@resultBuilder
+struct SettingGroupSectionBuilder {
+    static func buildBlock(_ components: TSKViewWrapper.SettingGroupData...) -> [TSKViewWrapper.SettingGroupData] {
+        components
+    }
+}
