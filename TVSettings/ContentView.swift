9ca15532d3d05b19339824a54fecc9e900d56d7e
zhrispineda <zhris@zhr.is>
2025-11-16 11:17:34 -0600

    Add sleepNow function

--- TVSettings/ContentView.swift
+++ TVSettings/ContentView.swift
@@ -33,10 +33,28 @@ struct ContentView: View {
                 
                 DTSection()
                 
-                TSKActionItem("SleepTitle") {}
+                TSKActionItem("SleepTitle") {
+                    sleepNow()
+                }
             }
         }
         .ignoresSafeArea()
+    }
+    
+    /// This will send a sleep signal to Apple TV through PineBoard/PineBoardServices.
+    /// But because this application is missing required entitlements, the sleep request will fail.
+    ///
+    /// Missing entitlements might include:
+    ///
+    /// `com.apple.appletv.pbs.allow-sleep`
+    private func sleepNow() {
+        guard
+            let cls = NSClassFromString("PBSSystemService") as? NSObject.Type,
+            let shared = cls.perform(NSSelectorFromString("sharedInstance"))?.takeUnretainedValue() as? NSObject
+        else { return }
+
+        shared.perform(NSSelectorFromString("sleepSystemForReason:"), with: "UserSettings")
+        print("Triggered system sleep.")
     }
 }
 
