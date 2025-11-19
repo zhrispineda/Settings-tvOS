bf41a989bf3aeaeea7199a5869e92048d6711f2e
zhrispineda <zhris@zhr.is>
2025-11-18 21:33:34 -0600

    Update SetupSection to add reboot function

--- TVSettings/Sections/SetupSection.swift
+++ TVSettings/Sections/SetupSection.swift
@@ -18,8 +18,26 @@ struct SetupSection: SettingsSectionBuilder {
             TSKSection("SetupMaintenanceSectionTitle") {
                 TSKItem("SoftwareUpdateTitle") {}
                 TSKItem("ResetTitle") {}
-                TSKActionItem("RestartTitle") {}
+                TSKActionItem("RestartTitle") {
+                    reboot()
+                }
             }
         }
+    }
+    
+    /// This will send a reboot signal to Apple TV through PineBoard/PineBoardServices.
+    /// But because this application is missing required entitlements, the sleep request will fail.
+    ///
+    /// Missing entitlements might include:
+    ///
+    /// `com.apple.appletv.pbs.allow-reboot`
+    private func reboot() {
+        guard
+            let cls = NSClassFromString("PBSSystemService") as? NSObject.Type,
+            let shared = cls.perform(NSSelectorFromString("sharedInstance"))?.takeUnretainedValue() as? NSObject
+        else { return }
+
+        shared.perform(NSSelectorFromString("reboot"))
+        print("-[PBSSystemService reboot]")
     }
 }
